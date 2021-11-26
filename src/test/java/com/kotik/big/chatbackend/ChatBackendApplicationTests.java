fe13d1e76c5069ba413090edb21ff2ad12014309
mirout <misharuz25@gmail.com>
2021-11-26 21:17:00 +0300

    Init commit

---
+++ src/test/java/com/kotik/big/chatbackend/ChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotik.big.chatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
